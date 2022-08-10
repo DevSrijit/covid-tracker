351fa5bf9a0e9fd41042546675b3db04dc512380
OptimalBlock489 <89468732+OptimalBlock489@users.noreply.github.com>
2022-08-10 11:35:45 +0530

    Add files via upload

---
+++ build/precache-manifest.950541c6306ac88d6d822a7d5d40a0cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c5e1a6653b032b9695278c6dcba70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1903ce568d72ab84eb64",
+    "url": "/static/css/2.1902b9f2.chunk.css"
+  },
+  {
+    "revision": "d1c92a5c1f3e11a14e44",
+    "url": "/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "1903ce568d72ab84eb64",
+    "url": "/static/js/2.68fd86e4.chunk.js"
+  },
+  {
+    "revision": "0cbfc08f72e2baa7949cb8cd737bd283",
+    "url": "/static/js/2.68fd86e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1c92a5c1f3e11a14e44",
+    "url": "/static/js/main.8a8cc619.chunk.js"
+  },
+  {
+    "revision": "dd7b6dd6b48561cca9ab",
+    "url": "/static/js/runtime-main.8c9d7948.js"
+  }
+]);
